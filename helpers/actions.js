5f1886ec57f8f5422c315e8e256941e62f88de74
Jefferson Dantas <jeffersonn.jjdaj@gmail.com>
2021-02-21 18:33:25 -0300

    Commit

--- helpers/actions.js
+++ helpers/actions.js
@@ -25,7 +25,7 @@ module.exports = {
 				contentType: file.mimetype
 			})
 			ids.push(img)
-			fs.unlink(__dirname + '/uploads/' + file.filename, () => {})
+			fs.unlink(__dirname + '/../uploads/' + file.filename, () => {})
 		})
 		res.json({ids})
 		
