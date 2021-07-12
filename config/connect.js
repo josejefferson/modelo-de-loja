34ed6c898f99032ef9219d80bdda44299265d74a
Jefferson Dantas <jeffersonn.jjdaj@gmail.com>
2021-07-12 14:43:28 -0300

    Testando classes no SCSS

--- config/connect.js
+++ config/connect.js
@@ -3,7 +3,7 @@ const LOCAL_DB = 'mongodb://localhost'
 const TEST_DB = 'mongodb+srv://eu:jefftestesjeff@cluster0.et1uf.mongodb.net/Cluster0?retryWrites=true&w=majority'
 const REMOTE_DB = ''
 
-module.exports = mongoose.connect(LOCAL_DB, {
+module.exports = mongoose.connect(TEST_DB, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 	useCreateIndex: true
