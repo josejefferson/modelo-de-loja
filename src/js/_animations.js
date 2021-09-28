bd0fbe937278eac1e87743417c8e659b2566b521
Jefferson Dantas <jeffersonn.jjdaj@gmail.com>
2021-09-28 14:53:52 -0300

    Animações

--- src/js/_animations.js
+++ src/js/_animations.js
@@ -1,15 +1,42 @@
-angular.module('store').animation('.request', () => ({ enter: anim.open, leave: anim.close }))
+angular.module('store').animation('.store-requests-client', () => ({ enter: anim.openSlideX, leave: anim.closeSlideX }))
+angular.module('store').animation('.store-requests-client-product', () => ({ enter: anim.open, leave: anim.close }))
 
 const anim = {
-	open: (el, done) => {
-		console.log('ANIMATION')
-		el.stop(true, false).css({ display: 'none', opacity: 0 }).slideDown(100).fadeTo(300, 1, done)
+	openSlideX: (el, done) => {
+		done()
 		return done
 	},
-	
+	closeSlideX: (el, done) => {
+		console.log('Closing')
+		anime.timeline({
+			targets: el[0],
+			easing: 'linear',
+			complete: done
+		}).add({
+			opacity: 0,
+			scale: 0.5,
+			duration: 150,
+			complete: () => el[0].style.setProperty('overflow', 'hidden', 'important')
+		}).add({
+			width: 0,
+			height: 0,
+			marginRight: 0,
+			duration: 200
+		})
+
+		return done
+	},
+	open: (el, done) => {
+		console.log('ANIMATION')
+		done()
+		// el.stop(true, false).css({ display: 'none', opacity: 0 }).slideDown(1000).fadeTo(3000, 1, done)
+		return done
+	},
+
 	close: (el, done) => {
 		console.log('ANIMATION')
-		el.stop(true, false).fadeTo(500, 0).slideUp(100, done)
+		done()
+		// el.stop(true, false).fadeTo(5000, 0).slideUp(1000, done)
 		return done
 	},
 }
