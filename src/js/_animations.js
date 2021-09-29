f1ae9186cacce2d60d81a5edbbf4d18fbfd695e7
Jefferson Dantas <jeffersonn.jjdaj@gmail.com>
2021-09-29 08:04:26 -0300

    Commit

--- src/js/_animations.js
+++ src/js/_animations.js
@@ -1,5 +1,5 @@
-angular.module('store').animation('.store-requests-client', () => ({ enter: anim.openSlideX, leave: anim.closeSlideX }))
-angular.module('store').animation('.store-requests-client-product', () => ({ enter: anim.openSlideY, leave: anim.closeSlideY }))
+angular.module('store').animation('.store-requests-client', () => ({ enter: anim.openSlide, leave: anim.closeSlide }))
+angular.module('store').animation('.store-requests-client-product', () => ({ enter: anim.openSlide, leave: anim.closeSlide }))
 
 const anim = {
 	openSlideX: (el, done) => {
@@ -36,80 +36,24 @@ const anim = {
 		return done
 	},
 
-	closeSlideX: (el, done) => {
-		anime.timeline({
-			targets: el[0],
-			easing: 'easeInOutQuad',
-			complete: done
-		}).add({
-			opacity: 0,
-			scale: 0.5,
-			duration: 150,
-			complete() {
-				el[0].style.setProperty('overflow', 'hidden', 'important')
-			}
-		}).add({
-			width: 0,
-			height: 0,
-			marginRight: 0,
-			duration: 200
-		})
-
+	openSlide: (el, done) => {
+		el[0].classList.add('animation-pre-open')
+		setTimeout(() => {
+			el[0].classList.add('animation-open')
+			setTimeout(() => {
+				el[0].style.width = el[0].clientWidth + 'px'
+				el[0].style.height = el[0].clientHeight + 'px'
+			}, 0)
+		}, 0)
+		setTimeout(done, 3500)
 		return done
 	},
 
-	openSlideY: (el, done) => {
-		const style = {...getComputedStyle(el[0])}
-		el[0].style.height = '0'
-		el[0].style.opacity = '0'
-		el[0].style.paddingTop = '0'
-		el[0].style.paddingBottom = '0'
-		el[0].style.setProperty('overflow', 'hidden', 'important')
-
-		anime.timeline({
-			targets: el[0],
-			easing: 'easeInOutQuad',
-			complete: done
-		}).add({
-			height: style.height,
-			paddingTop: style.paddingTop,
-			paddingBottom: style.paddingBottom,
-			duration: 200,
-			complete() {
-				el[0].style.overflow = null
-			}
-		}).add({
-			opacity: style.opacity,
-			duration: 150,
-			complete() {
-				el[0].style.height = null
-				el[0].style.opacity = null
-				el[0].style.paddingTop = null
-				el[0].style.paddingBottom = null
-			}
-		})
-
-		return done
-	},
-
-	closeSlideY: (el, done) => {
-		anime.timeline({
-			targets: el[0],
-			easing: 'easeInOutQuad',
-			complete: done
-		}).add({
-			opacity: 0,
-			duration: 150,
-			complete() {
-				el[0].style.setProperty('overflow', 'hidden', 'important')
-			}
-		}).add({
-			height: 0,
-			paddingTop: 0,
-			paddingBottom: 0,
-			duration: 200
-		})
-
+	closeSlide: (el, done) => {
+		el[0].style.width = el[0].clientWidth + 'px'
+		el[0].style.height = el[0].clientHeight + 'px'
+		setTimeout(() => el[0].classList.add('animation-close'), 0)
+		setTimeout(done, 350)
 		return done
 	}
 }
